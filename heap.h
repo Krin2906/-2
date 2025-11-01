999597355349779b4de575b9e9a3e23bba4bc577
krin29 <krinconsalinas1184@sdsu.edu>
2025-11-01 00:20:36 -0700

    Implement MinHeap and heapify operations

--- heap.h
+++ heap.h
@@ -1,6 +1,5 @@
-//
-// Created by Manju Muralidharan on 10/19/25.
-//
+// Written by Manju Muralidharan on 10/19/25
+// A quick & clean MinHeap implementation for managing indices with weights
 
 #ifndef HEAP_H
 #define HEAP_H
@@ -12,24 +11,97 @@ struct MinHeap {
     int data[64];
     int size;
 
-    MinHeap() { size = 0; }
-
-    void push(int idx, int weightArr[]) {
-        // TODO: insert index at end of heap, restore order using upheap()
+    // Default constructor nothing fancy
+    MinHeap() {
+        size = 0;
     }
 
-    int pop(int weightArr[]) {
-        // TODO: remove and return smallest index
-        // Replace root with last element, then call downheap()
-        return -1; // placeholder
+    // Push new index (with its weight reference) into the heap
+    void push(int index, int weights[]) {
+        if (size >= 64) {
+            cerr << "[warn] Heap full! Ignoring push for index " << index << endl;
+            return;
+        }
+        data[size] = index;
+        upheap(size, weights);
+        size++;
     }
 
-    void upheap(int pos, int weightArr[]) {
-        // TODO: swap child upward while smaller than parent
+    // pop the smallest (root) element
+    int pop(int weights[]) {
+        if (size == 0) {
+            //exception here???
+            return -1;
+        }
+        int minIdx = data[0];
+        data[0] = data[size - 1];
+        size--;
+
+        if (size > 0)
+            downheap(0, weights);
+
+        return minIdx;
     }
 
-    void downheap(int pos, int weightArr[]) {
-        // TODO: swap parent downward while larger than any child
+    // Bubble upward to maintain heap order
+    void upheap(int pos, int weights[]) {
+        while (pos > 0) {
+            int parent = (pos - 1) / 2;
+            int currIdx = data[pos];
+            int parentIdx = data[parent];
+
+            bool needSwap = false;
+
+            if (weights[currIdx] < weights[parentIdx]) // Smaller weight wins tie goes to smaller index
+
+                needSwap = true;
+            else if (weights[currIdx] == weights[parentIdx] && currIdx < parentIdx)
+                needSwap = true;
+            //postion are swapped
+
+            if (needSwap) {
+                int tmp = data[pos];
+                data[pos] = data[parent];
+                data[parent] = tmp;
+                pos = parent;
+            } else {
+                break;
+            }
+        }
+    }
+
+    // Bubble downward if a child is smaller
+    void downheap(int pos, int weights[]) {
+        while (true) {
+            int left = 2 * pos + 1;
+            int right = 2 * pos + 2;
+            int smallest = pos;
+
+            if (left < size) {
+                int leftIdx = data[left];
+                int smallestIdx = data[smallest];
+                if (weights[leftIdx] < weights[smallestIdx] ||
+                    (weights[leftIdx] == weights[smallestIdx] && leftIdx < smallestIdx))
+                    smallest = left;
+            }
+
+            if (right < size) {
+                int rightIdx = data[right];
+                int smallestIdx = data[smallest];
+                if (weights[rightIdx] < weights[smallestIdx] ||
+                    (weights[rightIdx] == weights[smallestIdx] && rightIdx < smallestIdx))
+                    smallest = right;
+            }
+
+            if (smallest != pos) {
+                int temp = data[pos];
+                data[pos] = data[smallest];
+                data[smallest] = temp;
+                pos = smallest;
+            } else {
+                break;
+            }
+        }
     }
 };
 
